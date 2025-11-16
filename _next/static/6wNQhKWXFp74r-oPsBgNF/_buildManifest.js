c0c4d2ff945337bbe4d0e700511b8d8f820cc3a9
Anıl Eren Ergin <55100250+anilerenergin@users.noreply.github.com>
2025-11-16 06:40:49 +0300

    Updates

--- _next/static/6wNQhKWXFp74r-oPsBgNF/_buildManifest.js
+++ _next/static/6wNQhKWXFp74r-oPsBgNF/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/demo-crm//_next/:path+",
+        "destination": "/demo-crm/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
